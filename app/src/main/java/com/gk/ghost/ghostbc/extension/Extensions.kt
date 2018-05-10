4199430200dfc2f55208fae023fb60205228ed62
gozde.kaval@gmail.com <Fatistan.git123>
2018-05-10 22:00:10 +0300

    Add inflater extension

---
+++ app/src/main/java/com/gk/ghost/ghostbc/extension/Extensions.kt
@@ -0,0 +1,13 @@
+package com.gk.ghost.ghostbc.extension
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * Created by Gozde Kaval on 5/10/2018.
+ */
+
+fun ViewGroup.inflate(layoutId: Int, attachToRoot: Boolean = false): View {
+    return LayoutInflater.from(context).inflate(layoutId, this, attachToRoot)
+}
